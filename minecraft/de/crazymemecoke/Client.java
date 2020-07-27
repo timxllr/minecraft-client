2d82b28b13e01f2557e967ca9c8b4f3030510864
CrazyMemeCoke <CrazyMemeCoke@gmail.com>
2020-07-27 03:41:03 +0200

    Cleaned up Client class

--- minecraft/de/crazymemecoke/Client.java
+++ minecraft/de/crazymemecoke/Client.java
@@ -1,19 +1,14 @@
 package de.crazymemecoke;
 
 import de.Hero.clickgui.ClickGUI;
-import de.Hero.settings.Setting;
 import de.Hero.settings.SettingsManager;
 import de.crazymemecoke.command.CommandManager;
-import de.crazymemecoke.module.Module;
 import de.crazymemecoke.module.ModuleManager;
 import de.crazymemecoke.notification.Notification;
-import de.crazymemecoke.notification.NotificationManager;
-import de.crazymemecoke.ui.GuiIngameHook;
 import de.crazymemecoke.ui.alts.AltManager;
 import de.crazymemecoke.ui.fontmanager.FontManager;
 import net.minecraft.client.Minecraft;
 
-import java.awt.*;
 import java.io.File;
 
 public class Client {
@@ -21,12 +16,8 @@ public class Client {
     private final static Client instance = new Client();
     private final Minecraft mc = Minecraft.getMinecraft();
 
-    private final Color clientColor = new Color(255, 124, 25, 255);
-    private final Color clientColorDarker = new Color(183, 74, 23, 255);
-
     // Naming Stuff
     private final String clientName = "Lifetime";
-    private final String clientNamePrefix = "L";
     private final String clientVersion = "b1";
     private final String clientAuthor = "CrazyMemeCoke";
     private final String clientPrefix = ".";
@@ -34,21 +25,13 @@ public class Client {
     // Paths of Files
     private final String clientBackground = "client/background.jpg";
 
-    private File raidriarDir;
     private ModuleManager moduleManager;
     private CommandManager commandManager;
-    private AltManager altManager;
     private SettingsManager setmgr;
     private ClickGUI clickgui;
-    //private CSGOGui clickgui;
     private FontManager fontManager;
-    private AltManager altMgr;
     private File clientDir;
-    private NotificationManager notificationManager;
     private Notification notification;
-    private GuiIngameHook guiIngameHook;
-
-    private Setting colorMode;
 
     public void startClient() {
         clientDir = new File(Minecraft.getMinecraft().mcDataDir + "/" + getClientName());
@@ -65,12 +48,11 @@ public class Client {
         moduleManager.loadBinds();
         commandManager = new CommandManager();
         AltManager.loadAlts();
-        raidriarDir = new File(Minecraft.getMinecraft().mcDataDir + File.separator + getClientName());
         clickgui = new ClickGUI();
         Runtime.getRuntime().addShutdownHook(new Thread(this::onShutdown));
     }
 
-    public static final Client getInstance() {
+    public static Client getInstance() {
         return instance;
     }
 
@@ -101,10 +83,6 @@ public class Client {
         return clientAuthor;
     }
 
-    public String getClientBackground() {
-        return clientBackground;
-    }
-
     public CommandManager getCommandManager() {
         return commandManager;
     }
@@ -125,28 +103,8 @@ public class Client {
         return clientPrefix;
     }
 
-    public Color getClientColor() {
-        return clientColor;
-    }
-
-    public Color getClientColorDarker() {
-        return clientColorDarker;
-    }
-
-    public Setting getSettings(String setting, Module mod) {
-        return Client.getInstance().getSetmgr().getSettingByName(setting, mod);
-    }
-
-    public Setting getColorMode() {
-        return colorMode;
-    }
-
     public FontManager getFontManager() {
         return fontManager;
-    }
-
-    public NotificationManager getNotificationManager() {
-        return notificationManager;
     }
 
     public Notification getNotification() {
