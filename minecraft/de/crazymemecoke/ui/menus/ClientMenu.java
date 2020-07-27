84eec0f86d4db0094ecf3f44861ea8c40f67f772
CrazyMemeCoke <CrazyMemeCoke@gmail.com>
2020-07-27 03:35:04 +0200

    Reworked Client Menu - Cleaned up old code - Added secret key combination for First Usage menu (LCONTROL + LSHIFT + X)

--- minecraft/de/crazymemecoke/ui/menus/ClientMenu.java
+++ minecraft/de/crazymemecoke/ui/menus/ClientMenu.java
@@ -20,56 +20,57 @@ import java.util.Random;
 public class ClientMenu extends GuiScreen implements GuiYesNoCallback {
 
     public void initGui() {
-        int i = 24;
-        int j = this.height / 4 + 48;
-
-        buttonList.add(new GuiButton(0, width - 120, 0, 120, 20, "Zurück"));
-        buttonList.add(new GuiButton(1, width - 120, 40, 120, 20, "Alt Login"));
-        buttonList.add(new GuiButton(2, width - 120, 60, 120, 20, "Session Stealer"));
-        buttonList.add(new GuiButton(3, width - 120, 80, 120, 20, "Alt Manager"));
-        buttonList.add(new GuiButton(4, width - 120, 100, 120, 20, "Changelog"));
+        buttonList.add(new GuiButton(1, width / 2 - 50, height / 2 - 30, 120, 20, "Alt Login"));
+        buttonList.add(new GuiButton(3, width / 2 - 50, height / 2 - 60, 120, 20, "Alt Manager"));
+        buttonList.add(new GuiButton(0, width / 2 - 50, height / 2, 120, 20, "Zurück"));
+        buttonList.add(new GuiButton(2, width / 2 - 50, height / 2 + 30, 120, 20, "Session Stealer"));
+        buttonList.add(new GuiButton(4, width / 2 - 50, height / 2 + 60, 120, 20, "Changelog"));
     }
 
     protected void actionPerformed(GuiButton button) throws IOException {
         if (button.id == 0) {
-            this.mc.displayGuiScreen(new GuiMainMenu());
+            mc.displayGuiScreen(new GuiMainMenu());
         }
         if (button.id == 1) {
-            this.mc.displayGuiScreen(new AltLogin(this));
+            mc.displayGuiScreen(new AltLogin(this));
         }
         if (button.id == 2) {
-            this.mc.displayGuiScreen(new SessionStealer(this));
+            mc.displayGuiScreen(new SessionStealer(this));
         }
         if (button.id == 3) {
-            this.mc.displayGuiScreen(new GuiAltManager(this));
+            mc.displayGuiScreen(new GuiAltManager(this));
         }
         if (button.id == 4) {
             mc.displayGuiScreen(new Changelog());
         }
     }
 
+    @Override
+    protected void keyTyped(char typedChar, int keyCode) throws IOException {
+        if (Keyboard.isKeyDown(Keyboard.KEY_LCONTROL) && Keyboard.isKeyDown(Keyboard.KEY_LSHIFT) && Keyboard.isKeyDown(Keyboard.KEY_X)) {
+            mc.displayGuiScreen(new FirstUsage());
+        }
+    }
+
     public void drawScreen(int mouseX, int mouseY, float partialTicks) {
         GlStateManager.disableAlpha();
         GlStateManager.enableAlpha();
-        Tessellator tessellator = Tessellator.getInstance();
-        WorldRenderer worldrenderer = tessellator.getWorldRenderer();
-        int i = 274;
-        int j = this.width / 2 - i / 2;
-        int k = 30;
-        this.drawGradientRect(0, 0, this.width, this.height, -2130706433, 16777215);
-        this.drawGradientRect(0, 0, this.width, this.height, 0, Integer.MIN_VALUE);
+        drawGradientRect(0, 0, width, height, -2130706433, 16777215);
+        drawGradientRect(0, 0, width, height, 0, Integer.MIN_VALUE);
         GlStateManager.color(1.0F, 1.0F, 1.0F, 1.0F);
 
-        ScaledResolution scaledRes = new ScaledResolution(this.mc);
+        ScaledResolution scaledRes = new ScaledResolution(mc);
         Gui.drawRect(0, 0, scaledRes.getScaledWidth(), scaledRes.getScaledHeight(), new Color(28, 26, 28).getRGB());
 
         GlStateManager.pushMatrix();
-        GlStateManager.translate((float) (this.width / 2 + 90), 70.0F, 0.0F);
+        GlStateManager.translate((float) (width / 2 + 90), 70.0F, 0.0F);
         GlStateManager.rotate(-20.0F, 0.0F, 0.0F, 1.0F);
         float f = 1.8F - MathHelper.abs(
                 MathHelper.sin((float) (Minecraft.getSystemTime() % 1000L) / 1000.0F * (float) Math.PI * 2.0F) * 0.1F);
         GlStateManager.scale(f, f, f);
         GlStateManager.popMatrix();
+
+        Client.getInstance().getFontManager().comfortaa50.drawStringWithShadow(Client.getInstance().getClientName() + " Menü", width / 2 - 80, 20, -1);
 
         float scale = 5.0F;
         GL11.glScalef(scale, scale, scale);
