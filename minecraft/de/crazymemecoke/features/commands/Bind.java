2fb141e19268c1951197689df04eb4b2b131bacd
CrazyMemeCoke <CrazyMemeCoke@gmail.com>
2020-08-04 00:32:20 +0200

    Small overhaul - Changed name of ArrayList of all friends (Friends CMD) - Optimized Bind Command (removed old keyNameToKeyCode method)

--- minecraft/de/crazymemecoke/features/commands/Bind.java
+++ minecraft/de/crazymemecoke/features/commands/Bind.java
@@ -11,58 +11,58 @@ import de.crazymemecoke.utils.Wrapper;
 
 public class Bind extends Command {
 
-	String syntax = ".bind set <module> <key> | .bind list | .bind clear";
+    String syntax = ".bind <module> <key> | .bind list | .bind clear";
 
-	@Override
-	public void execute(String[] args) {
-		ModuleManager moduleManager = Client.getInstance().getModuleManager();
-		if (args.length == 3) {
-			if (args[0].equalsIgnoreCase("set")) {
-				Module mod = moduleManager.getModByName(args[1]);
-				if (mod == null) {
-					Notify.chatMessage("Module " + args[1] + " nicht gefunden.");
-					return;
-				}
+    @Override
+    public void execute(String[] args) {
+        ModuleManager moduleManager = Client.getInstance().getModuleManager();
+        if (args.length == 2) {
+            if (Client.getInstance().getModuleManager().getModByName(args[0]) != null) {
+                Module mod = moduleManager.getModByName(args[0]);
+                if (mod == null) {
+                    Notify.chatMessage("Module " + args[0] + " nicht gefunden.");
+                    return;
+                }
 
-				int bind = moduleManager.convertKeyCodeToInt(args[2]);
+                int bind = Keyboard.getKeyIndex(args[1]);
 
-				if (bind == 0) {
-					Notify.chatMessage("Key " + args[2] + " nicht gefunden.");
-					return;
-				}
+                if (bind == 0) {
+                    Notify.chatMessage("Key " + args[1] + " nicht gefunden.");
+                    return;
+                }
 
-				mod.setBind(bind);
-				moduleManager.saveBinds();
-				Notify.chatMessage("Module " + args[1] + " wurde auf " + args[2] + " gebunden.");
-				Wrapper.mc.thePlayer.playSound("random.anvil_use", 1f, 1f);
-			} else {
-				Notify.chatMessage(syntax);
-			}
-		} else if (args.length == 1) {
-			if (args[0].equalsIgnoreCase("list")) {
-				Notify.chatMessage("Alle Keybinds (Format: MOD : KEY):");
-				for (Module mod : Client.getInstance().getModuleManager().getModules()) {
-					if(!(mod.getBind() == 0)){
-						Notify.chatMessage(mod.getName() + " : " + Keyboard.getKeyName(mod.getBind()));
-					}
-				}
-			} else if(args[0].equalsIgnoreCase("clear")) {
-				for(Module mod : Client.getInstance().getModuleManager().getModules()) {
-					mod.setBind(0);
-				}
-				Notify.chatMessage("Es wurden alle Keybinds gelöscht.");
-			}else {
-				Notify.chatMessage(syntax);
-			}
-		} else {
-			Notify.chatMessage(syntax);
-		}
+                mod.setBind(bind);
+                moduleManager.saveBinds();
+                Notify.chatMessage("Module " + args[0] + " wurde auf " + args[1] + " gebunden.");
+                Wrapper.mc.thePlayer.playSound("random.anvil_use", 1f, 1f);
+            } else {
+                Notify.chatMessage(syntax);
+            }
+        } else if (args.length == 1) {
+            if (args[0].equalsIgnoreCase("list")) {
+                Notify.chatMessage("Alle Keybinds (Format: MOD : KEY):");
+                for (Module mod : Client.getInstance().getModuleManager().getModules()) {
+                    if (!(mod.getBind() == 0)) {
+                        Notify.chatMessage(mod.getName() + " : " + Keyboard.getKeyName(mod.getBind()));
+                    }
+                }
+            } else if (args[0].equalsIgnoreCase("clear")) {
+                for (Module mod : Client.getInstance().getModuleManager().getModules()) {
+                    mod.setBind(0);
+                }
+                Notify.chatMessage("Es wurden alle Keybinds gelöscht.");
+            } else {
+                Notify.chatMessage(syntax);
+            }
+        } else {
+            Notify.chatMessage(syntax);
+        }
 
-	}
+    }
 
-	@Override
-	public String getName() {
-		return "Bind";
-	}
+    @Override
+    public String getName() {
+        return "Bind";
+    }
 
 }
--- minecraft/de/crazymemecoke/features/commands/Friend.java
+++ minecraft/de/crazymemecoke/features/commands/Friend.java
@@ -8,34 +8,34 @@ import de.crazymemecoke.utils.Notify;
 public class Friend extends Command {
 
     String syntax = ".friend add <name> | .friend remove <name> | .friend list";
-    public static ArrayList<String> Friend = new ArrayList<String>();
+    public static ArrayList<String> friends = new ArrayList<String>();
 
     @Override
     public void execute(String[] args) {
         if (args.length == 1) {
             if (args[0].equalsIgnoreCase("list")) {
                 Notify.chatMessage("Deine Freunde:");
-                Notify.chatMessage(Friend.toString());
+                Notify.chatMessage(friends.toString());
             } else {
                 Notify.chatMessage(syntax);
             }
         } else {
             if (args.length == 2) {
                 if (args[0].equalsIgnoreCase("add")) {
-                    if (!Friend.contains(args[1])) {
-                        Friend.add(args[1]);
+                    if (!friends.contains(args[1])) {
+                        friends.add(args[1]);
                         Notify.chatMessage(args[1] + " wurde zu deiner Freundesliste hinzugefügt!");
                     } else {
-                        if (Friend.contains(args[1])) {
+                        if (friends.contains(args[1])) {
                             Notify.chatMessage(args[1] + " ist bereits in deiner Freundesliste!");
                         }
                     }
                 } else if (args[0].equalsIgnoreCase("remove")) {
-                    if (Friend.contains(args[1])) {
-                        Friend.remove(args[1]);
+                    if (friends.contains(args[1])) {
+                        friends.remove(args[1]);
                         Notify.chatMessage(args[1] + " wurde aus deiner Freundesliste entfernt!");
                     } else {
-                        if (!Friend.contains(args[1])) {
+                        if (!friends.contains(args[1])) {
                             Notify.chatMessage(args[1] + " ist nicht in deiner Freundesliste!");
                         }
                     }
