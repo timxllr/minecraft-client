088a30cad24a19ab479029bbdf0648b1fb228abe
CrazyMemeCoke <CrazyMemeCoke@gmail.com>
2020-08-06 16:02:48 +0200

    Added Delay for FastUse

--- minecraft/de/crazymemecoke/features/modules/player/FastUse.java
+++ minecraft/de/crazymemecoke/features/modules/player/FastUse.java
@@ -1,5 +1,7 @@
 package de.crazymemecoke.features.modules.player;
 
+import de.crazymemecoke.Client;
+import de.crazymemecoke.manager.clickguimanager.settings.Setting;
 import org.lwjgl.input.Keyboard;
 
 import de.crazymemecoke.manager.modulemanager.Category;
@@ -19,50 +21,54 @@ import net.minecraft.util.EnumFacing;
 public class FastUse extends Module {
 
 	public FastUse() {
-		super("FastUse", Keyboard.KEY_NONE, Category.PLAYER, Rainbow.rainbow(1, 1).hashCode());
+		super("FastUse", Keyboard.KEY_NONE, Category.PLAYER, Rainbow.rainbow(1, 1).getRGB());
+
+		Client.getInstance().getSetmgr().rSetting(new Setting("Delay", this, 15, 0, 20, false));
 	}
 
 	boolean NCP = true;
 
 	@Override
 	public void onUpdate() {
-		if (this.getState()) {
+		double delay = Client.getInstance().getSetmgr().getSettingByName("Delay", this).getValDouble();
+
+		if (getState()) {
 			try {
-				if (isSword(this.mc.thePlayer.inventory.getCurrentItem().getItem())) {
+				if (isSword(mc.thePlayer.inventory.getCurrentItem().getItem())) {
 					return;
 				}
 			} catch (Exception e) {
 				return;
 			}
-			if (this.mc.thePlayer.getItemInUseDuration() > 15) {
+			if (mc.thePlayer.getItemInUseDuration() > delay) {
 				for (int i = 0; i < 20; i++) {
 					Minecraft.getMinecraft().thePlayer.sendQueue
-							.addToSendQueue(new C03PacketPlayer(this.mc.thePlayer.onGround));
+							.addToSendQueue(new C03PacketPlayer(mc.thePlayer.onGround));
 				}
-				if ((this.mc.thePlayer.getCurrentEquippedItem().getItem() instanceof ItemBow)) {
+				if ((mc.thePlayer.getCurrentEquippedItem().getItem() instanceof ItemBow)) {
 					if (!NCP) {
 						shootBow();
 						return;
 					}
-					this.mc.thePlayer.sendQueue.addToSendQueue(new C07PacketPlayerDigging(
+					mc.thePlayer.sendQueue.addToSendQueue(new C07PacketPlayerDigging(
 							C07PacketPlayerDigging.Action.RELEASE_USE_ITEM, new BlockPos(0, 0, 0), EnumFacing.UP));
-					this.mc.thePlayer.stopUsingItem();
+					mc.thePlayer.stopUsingItem();
 				} else {
-					this.mc.thePlayer.stopUsingItem();
+					mc.thePlayer.stopUsingItem();
 				}
 			}
 		}
 	}
 
 	public void shootBow() {
-		int item = this.mc.thePlayer.inventory.currentItem;
-		this.mc.thePlayer.sendQueue.addToSendQueue(new C08PacketPlayerBlockPlacement(new BlockPos(-1, -1, -1), 255,
-				this.mc.thePlayer.inventory.getCurrentItem(), 0.0F, 0.0F, 0.0F));
-		this.mc.thePlayer.sendQueue.addToSendQueue(new C09PacketHeldItemChange(item));
+		int item = mc.thePlayer.inventory.currentItem;
+		mc.thePlayer.sendQueue.addToSendQueue(new C08PacketPlayerBlockPlacement(new BlockPos(-1, -1, -1), 255,
+				mc.thePlayer.inventory.getCurrentItem(), 0.0F, 0.0F, 0.0F));
+		mc.thePlayer.sendQueue.addToSendQueue(new C09PacketHeldItemChange(item));
 		for (int i = 0; i < 20; i++) {
-			this.mc.thePlayer.sendQueue.addToSendQueue(new C03PacketPlayer(false));
+			mc.thePlayer.sendQueue.addToSendQueue(new C03PacketPlayer(false));
 		}
-		this.mc.thePlayer.sendQueue.addToSendQueue(new C07PacketPlayerDigging(
+		mc.thePlayer.sendQueue.addToSendQueue(new C07PacketPlayerDigging(
 				C07PacketPlayerDigging.Action.RELEASE_USE_ITEM, new BlockPos(0, 0, 0), EnumFacing.fromAngle(-1.0D)));
 	}
 
