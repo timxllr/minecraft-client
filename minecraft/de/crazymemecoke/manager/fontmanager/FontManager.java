c7fbaa7d03f5250466a7791dbf0f3f0631bc7e77
CrazyMemeCoke <CrazyMemeCoke@gmail.com>
2020-08-20 21:44:11 +0200

    Added new Fonts (Ambien, Vortex, Arial)

--- minecraft/de/crazymemecoke/manager/fontmanager/FontManager.java
+++ minecraft/de/crazymemecoke/manager/fontmanager/FontManager.java
@@ -18,17 +18,11 @@ public class FontManager {
     public UnicodeFontRenderer comfortaa45;
     public UnicodeFontRenderer comfortaa50;
     public UnicodeFontRenderer notificationFont;
-    public UnicodeFontRenderer american100;
-    public UnicodeFontRenderer american66;
     public UnicodeFontRenderer verdana23;
     public UnicodeFontRenderer iconsSouthSt14;
     public UnicodeFontRenderer modernpics22;
     public UnicodeFontRenderer glyphyxOneNF19;
-    public UnicodeFontRenderer centuryGothic45;
-    public UnicodeFontRenderer centuryGothic30;
-    public UnicodeFontRenderer centuryGothic20;
     public UnicodeFontRenderer statusFont;
-    public UnicodeFontRenderer arialBold15;
     public UnicodeFontRenderer cabin18;
     public UnicodeFontRenderer cabin23;
     public UnicodeFontRenderer cabin35;
@@ -43,6 +37,27 @@ public class FontManager {
     public UnicodeFontRenderer bebasNeue40;
     public UnicodeFontRenderer bebasNeue45;
     public UnicodeFontRenderer bebasNeue50;
+    public UnicodeFontRenderer roboto20;
+    public UnicodeFontRenderer roboto25;
+    public UnicodeFontRenderer raleWay20;
+    public UnicodeFontRenderer raleWay25;
+    public UnicodeFontRenderer esp30;
+    public UnicodeFontRenderer esp35;
+    public UnicodeFontRenderer ambien20;
+    public UnicodeFontRenderer ambien25;
+    public UnicodeFontRenderer ambien30;
+    public UnicodeFontRenderer ambien35;
+    public UnicodeFontRenderer ambien40;
+    public UnicodeFontRenderer ambien45;
+    public UnicodeFontRenderer ambien50;
+    public UnicodeFontRenderer vortex20;
+    public UnicodeFontRenderer vortex25;
+    public UnicodeFontRenderer vortex30;
+    public UnicodeFontRenderer vortex35;
+    public UnicodeFontRenderer vortex40;
+    public UnicodeFontRenderer vortex45;
+    public UnicodeFontRenderer vortex50;
+    public UnicodeFontRenderer arial20;
 
     public void initFonts() {
         bebasNeue20 = new UnicodeFontRenderer(getFont("BebasNeue", 20, FontExtension.OTF), true, 8);
@@ -65,6 +80,27 @@ public class FontManager {
         cabin18 = new UnicodeFontRenderer(getFont("Cabin", 18, FontExtension.TTF), true, 8);
         cabin23 = new UnicodeFontRenderer(getFont("Cabin", 23, FontExtension.TTF), true, 8);
         cabin35 = new UnicodeFontRenderer(getFont("Cabin", 35, FontExtension.TTF), true, 8);
+        roboto20 = new UnicodeFontRenderer(getFont("Roboto-Light", 20, FontExtension.TTF), true, 8);
+        roboto25 = new UnicodeFontRenderer(getFont("Roboto-Light", 25, FontExtension.TTF), true, 8);
+        raleWay20 = new UnicodeFontRenderer(getFont("Raleway-Light", 20, FontExtension.TTF), true, 8);
+        raleWay25 = new UnicodeFontRenderer(getFont("Raleway-Light", 25, FontExtension.TTF), true, 8);
+        esp30 = new UnicodeFontRenderer(getFont("esp", 30, FontExtension.TTF), true, 8);
+        esp35 = new UnicodeFontRenderer(getFont("esp", 35, FontExtension.TTF), true, 8);
+        ambien20 = new UnicodeFontRenderer(getFont("fifawelcome1.3", 20, FontExtension.TTF), true, 8);
+        ambien25 = new UnicodeFontRenderer(getFont("fifawelcome1.3", 25, FontExtension.TTF), true, 8);
+        ambien30 = new UnicodeFontRenderer(getFont("fifawelcome1.3", 30, FontExtension.TTF), true, 8);
+        ambien35 = new UnicodeFontRenderer(getFont("fifawelcome1.3", 35, FontExtension.TTF), true, 8);
+        ambien40 = new UnicodeFontRenderer(getFont("fifawelcome1.3", 40, FontExtension.TTF), true, 8);
+        ambien45 = new UnicodeFontRenderer(getFont("fifawelcome1.3", 45, FontExtension.TTF), true, 8);
+        ambien50 = new UnicodeFontRenderer(getFont("fifawelcome1.3", 50, FontExtension.TTF), true, 8);
+        vortex20 = new UnicodeFontRenderer(getFont("DANUBE", 20, FontExtension.TTF), true, 8);
+        vortex25 = new UnicodeFontRenderer(getFont("DANUBE", 25, FontExtension.TTF), true, 8);
+        vortex30 = new UnicodeFontRenderer(getFont("DANUBE", 30, FontExtension.TTF), true, 8);
+        vortex35 = new UnicodeFontRenderer(getFont("DANUBE", 35, FontExtension.TTF), true, 8);
+        vortex40 = new UnicodeFontRenderer(getFont("DANUBE", 40, FontExtension.TTF), true, 8);
+        vortex45 = new UnicodeFontRenderer(getFont("DANUBE", 45, FontExtension.TTF), true, 8);
+        vortex50 = new UnicodeFontRenderer(getFont("DANUBE", 50, FontExtension.TTF), true, 8);
+        arial20 = new UnicodeFontRenderer(getFont("Arial", 20, FontExtension.TTF), true, 8);
     }
 
     private static Font getFont(String name, int size, FontExtension fe) {
