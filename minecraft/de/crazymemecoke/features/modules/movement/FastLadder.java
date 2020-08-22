c8d67d9ea4deca79021b5070c4109ea0b880f0c4
CrazyMemeCoke <CrazyMemeCoke@gmail.com>
2020-08-23 00:23:59 +0200

    Reworked FastLadder module - Added Mode selector (Vanilla, AAC) - Added Slider for Vanilla speed - Added Slider for AAC speed

--- minecraft/de/crazymemecoke/features/modules/movement/FastLadder.java
+++ minecraft/de/crazymemecoke/features/modules/movement/FastLadder.java
@@ -1,22 +1,59 @@
 package de.crazymemecoke.features.modules.movement;
 
+import de.crazymemecoke.Client;
+import de.crazymemecoke.manager.clickguimanager.settings.Setting;
 import de.crazymemecoke.manager.modulemanager.Category;
 import de.crazymemecoke.manager.modulemanager.Module;
-import de.crazymemecoke.utils.render.Rainbow;
 import org.lwjgl.input.Keyboard;
+
+import java.util.ArrayList;
 
 public class FastLadder extends Module {
 
-	public FastLadder() {
-		super("FastLadder", Keyboard.KEY_NONE, Category.MOVEMENT, -1);
-	}
+    ArrayList<String> mode = new ArrayList<>();
 
-	@Override
-	public void onUpdate() {
-		if (getState()) {
-			if ((mc.thePlayer.isOnLadder()) && (mc.thePlayer.isCollidedHorizontally)) {
-				mc.thePlayer.motionY = 0.28D;
-			}
-		}
-	}
+    public FastLadder() {
+        super("FastLadder", Keyboard.KEY_NONE, Category.MOVEMENT, -1);
+
+        mode.add("Vanilla");
+        mode.add("AAC");
+
+        Client.instance().setMgr().newSetting(new Setting("Mode", this, "Vanilla", mode));
+        Client.instance().setMgr().newSetting(new Setting("Vanilla Speed", this, 0.4, 0.1, 1.0, false));
+        Client.instance().setMgr().newSetting(new Setting("AAC Speed", this, 0.2, 0.1, 1.0, false));
+    }
+
+    @Override
+    public void onUpdate() {
+        String mode = Client.instance().setMgr().getSettingByName("Mode", this).getMode();
+
+        if (getState()) {
+            switch (mode) {
+                case "vanilla": {
+                    doVanilla();
+                    break;
+                }
+                case "aac": {
+                    doAAC();
+                    break;
+                }
+            }
+        }
+    }
+
+    private void doAAC() {
+        double speed = Client.instance().setMgr().getSettingByName("AAC Speed", this).getNum();
+
+        if ((mc.thePlayer.isOnLadder()) && (mc.thePlayer.isCollidedHorizontally)) {
+            mc.thePlayer.motionY = speed;
+        }
+    }
+
+    private void doVanilla() {
+        double speed = Client.instance().setMgr().getSettingByName("Vanilla Speed", this).getNum();
+
+        if ((mc.thePlayer.isOnLadder()) && (mc.thePlayer.isCollidedHorizontally)) {
+            mc.thePlayer.motionY = speed;
+        }
+    }
 }
