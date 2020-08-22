b9199f2d859ba200f8749824a9f426f4c0f6a7eb
CrazyMemeCoke <CrazyMemeCoke@gmail.com>
2020-08-23 00:24:12 +0200

    Reworked Step module - Added NCP mode

--- minecraft/de/crazymemecoke/features/modules/movement/Step.java
+++ minecraft/de/crazymemecoke/features/modules/movement/Step.java
@@ -2,12 +2,13 @@ package de.crazymemecoke.features.modules.movement;
 
 import de.crazymemecoke.Client;
 import de.crazymemecoke.manager.clickguimanager.settings.Setting;
-import de.crazymemecoke.manager.clickguimanager.settings.SettingsManager;
 import de.crazymemecoke.manager.modulemanager.Category;
 import de.crazymemecoke.manager.modulemanager.Module;
 import org.lwjgl.input.Keyboard;
 
 import java.util.ArrayList;
+
+import static net.minecraft.network.play.client.C03PacketPlayer.C04PacketPlayerPosition;
 
 public class Step extends Module {
 
@@ -16,21 +17,46 @@ public class Step extends Module {
         ArrayList<String> mode = new ArrayList<>();
 
         mode.add("Vanilla");
+        mode.add("NCP");
 
         Client.instance().setMgr().newSetting(new Setting("Mode", this, "Vanilla", mode));
+        Client.instance().setMgr().newSetting(new Setting("Step Height", this, 1.0, 1.0, 10.0, false));
     }
 
-    SettingsManager sM = Client.instance().setMgr();
+    @Override
+    public void onDisable() {
+        mc.thePlayer.stepHeight = 0.5f;
+    }
 
     public void onUpdate() {
-        if (this.getState()) {
-            if (sM.getSettingByName("Mode", this).getMode().equalsIgnoreCase("Vanilla")) {
-                if (mc.thePlayer.isCollidedHorizontally && mc.thePlayer.onGround) {
-                    mc.thePlayer.jump();
-                } else {
-                    mc.thePlayer.stepHeight = 0.5F;
+        String mode = Client.instance().setMgr().getSettingByName("Mode", this).getMode();
+
+        if (getState()) {
+            switch (mode) {
+                case "vanilla": {
+                    doVanilla();
+                    break;
+                }
+                case "ncp": {
+                    doNCP();
+                    break;
                 }
             }
         }
+    }
+
+    private void doNCP() {
+        double stepHeight = Client.instance().setMgr().getSettingByName("Step Height", this).getNum();
+
+        final double posX = mc.thePlayer.posX;
+        final double posY = mc.thePlayer.posY;
+        final double posZ = mc.thePlayer.posZ;
+        mc.thePlayer.stepHeight = (float) stepHeight;
+        mc.thePlayer.sendQueue.addToSendQueue(new C04PacketPlayerPosition(posX, posY + 0.42, posZ, mc.thePlayer.onGround));
+        mc.thePlayer.sendQueue.addToSendQueue(new C04PacketPlayerPosition(posX, posY + 0.753, posZ, mc.thePlayer.onGround));
+    }
+
+    private void doVanilla() {
+        mc.thePlayer.jump();
     }
 }
