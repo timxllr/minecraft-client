fb0cded330652c9e257b1d05889334c7c9698de0
realfantacoke <CrazyMemeCoke@gmail.com>
2020-10-21 22:27:12 +0200

    Added auto-close option You can now dynamically choose, if the Client should automatically close the Chest container after it stole all items out of it, or not.
    Signed-off-by: realfantacoke <CrazyMemeCoke@gmail.com>

--- minecraft/de/crazymemecoke/features/modules/player/ChestStealer.java
+++ minecraft/de/crazymemecoke/features/modules/player/ChestStealer.java
@@ -7,7 +7,10 @@ import de.crazymemecoke.manager.events.impl.EventUpdate;
 import de.crazymemecoke.manager.modulemanager.Category;
 import de.crazymemecoke.manager.modulemanager.Module;
 import de.crazymemecoke.utils.time.TimeHelper;
+import net.minecraft.client.gui.inventory.GuiChest;
+import net.minecraft.inventory.Container;
 import net.minecraft.inventory.ContainerChest;
+import net.minecraft.item.ItemStack;
 import org.lwjgl.input.Keyboard;
 
 public class ChestStealer extends Module {
@@ -16,14 +19,14 @@ public class ChestStealer extends Module {
     public ChestStealer() {
         super("ChestStealer", Keyboard.KEY_NONE, Category.PLAYER, -1);
         Client.main().setMgr().newSetting(new Setting("Delay", this, 75, 0, 250, true));
+        Client.main().setMgr().newSetting(new Setting("Auto Close", this, true));
     }
 
     @Override
     public void onEvent(Event event) {
         if (event instanceof EventUpdate) {
             long delay = (long) Client.main().setMgr().settingByName("Delay", this).getNum();
-            if ((mc.thePlayer.openContainer != null)
-                    && ((mc.thePlayer.openContainer instanceof ContainerChest))) {
+            if (((mc.thePlayer.openContainer instanceof ContainerChest))) {
                 ContainerChest container = (ContainerChest) mc.thePlayer.openContainer;
                 for (int i = 0; i < container.getLowerChestInventory().getSizeInventory(); i++) {
                     if ((container.getLowerChestInventory().getStackInSlot(i) != null)
@@ -32,10 +35,22 @@ public class ChestStealer extends Module {
                         time.reset();
                     }
                 }
-                if (container.getInventory().isEmpty()) {
-                    mc.thePlayer.closeScreen();
+                if (isEmpty(container)) {
+                    if (Client.main().setMgr().settingByName("Auto Close", this).getBool()) {
+                        mc.thePlayer.closeScreen();
+                    }
                 }
             }
         }
+    }
+
+    private boolean isEmpty(ContainerChest chest) {
+        for (int i = 0; i < chest.inventorySlots.size() - 36; i++) {
+            final ItemStack item = chest.getSlot(i).getStack();
+            if (item != null) {
+                return false;
+            }
+        }
+        return true;
     }
 }
