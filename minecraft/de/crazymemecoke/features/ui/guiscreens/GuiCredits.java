dce7dfb35e3900311e1849664b512fce7cb04577
realfantacoke <CrazyMemeCoke@gmail.com>
2020-10-24 20:15:42 +0200

    Recoded entire Credits screen - Added new Credits - Optimized Font System - Changed Font Sizes
    Signed-off-by: realfantacoke <CrazyMemeCoke@gmail.com>

--- minecraft/de/crazymemecoke/features/ui/guiscreens/GuiCredits.java
+++ minecraft/de/crazymemecoke/features/ui/guiscreens/GuiCredits.java
@@ -16,17 +16,9 @@ import java.util.ArrayList;
 public class GuiCredits extends GuiScreen {
     public GuiScreen parent;
 
-    public static ArrayList altList = new ArrayList();
-    public static ArrayList guiSlotList = new ArrayList();
-    public static File altFile;
-
     FontManager fM = Client.main().fontMgr();
 
     private GuiScreen parentScreen;
-
-    public File getAltFile() {
-        return altFile;
-    }
 
     public GuiCredits(GuiScreen parentScreen) {
         parent = parentScreen;
@@ -35,6 +27,13 @@ public class GuiCredits extends GuiScreen {
     public void initGui() {
         Keyboard.enableRepeatEvents(true);
         buttonList.add(new GuiButton(1, width / 2 - 100, height - 30, 200, 20, "Zurück"));
+    }
+
+    @Override
+    protected void keyTyped(char typedChar, int keyCode) throws IOException {
+        if (Keyboard.isKeyDown(Keyboard.KEY_ESCAPE)) {
+            mc.displayGuiScreen(parentScreen);
+        }
     }
 
     public void onGuiClosed() {
@@ -51,30 +50,25 @@ public class GuiCredits extends GuiScreen {
         drawString(mc.fontRendererObj, "", width / 2 - 100, 79, 10526880);
 
         ScaledResolution sr = new ScaledResolution(Wrapper.mc);
-        if (Keyboard.isKeyDown(1)) {
-            mc.displayGuiScreen(parent);
-        }
 
         mc.getTextureManager().bindTexture(new ResourceLocation(Client.main().getClientBackground()));
         Gui.drawScaledCustomSizeModalRect(0, 0, 0.0F, 0.0F, sr.width(), sr.height(),
                 width, height, sr.width(), sr.height());
 
-        int x = width / 2 - 150;
-        int darkGray = -15658735;
-        int lightGray = -15066598;
         RenderUtils.drawRect(5, 5, width - 5, height - 5, new Color(0, 0, 0, 155).getRGB());
 
         String title = "Credits";
-        fM.cabin35.drawStringWithShadow(title, width / 2 - fM.cabin35.getStringWidth(title) / 2, 10, -1);
+        fM.font("Cabin", 35, Font.PLAIN).drawStringWithShadow(title, width / 2 - fM.cabin35.getStringWidth(title) / 2, 10, -1);
 
-        String c1 = "HeroCode - ClickGUI API";
-        String c2 = "Kriteax - Client Port from 1.8 to 1.8.8 | General Help";
-        String c3 = "Sam - Font System | General Help";
-        String c4 = "Nero - Fonts | Crasher | Circle ESP";
-        fM.cabin23.drawStringWithShadow(c1, width / 2 - fM.cabin23.getStringWidth(c1) / 2, height / 2 - 30, -1);
-        fM.cabin23.drawStringWithShadow(c2, width / 2 - fM.cabin23.getStringWidth(c2) / 2, height / 2 - 10, -1);
-        fM.cabin23.drawStringWithShadow(c3, width / 2 - fM.cabin23.getStringWidth(c3) / 2, height / 2 + 10, -1);
-        fM.cabin23.drawStringWithShadow(c4, width / 2 - fM.cabin23.getStringWidth(c4) / 2, height / 2 + 30, -1);
+        String credits = "HeroCode - ClickGUI API\n\n" +
+                "Kriteax - Client Port from 1.8 to 1.8.8 | General Help\n\n" +
+                "Sam - Font System | General Help\n\n" +
+                "Nero - Fonts | Crasher | Shaders | General, fresh SRC\n\n" +
+                "Kroko - Event System | ShaderESP | Aura Rotations | General Help\n\n" +
+                "W4z3d - Hit Animations | General Help\n\n" +
+                "Deleteboys - TrailESP | MotionGraph | Rainbow Method\n\n" +
+                "superblaubeere27 - Shader System";
+        fM.font("Cabin", 23, Font.PLAIN).drawStringWithShadow(credits, 10, 40, -1);
 
         super.drawScreen(posX, posY, f);
     }
