a317ccb8ac9fd72dfae59f325435e3a4d68d99d1
realfantacoke <CrazyMemeCoke@gmail.com>
2020-10-31 23:15:18 +0100

    Tried to fix NoSlow, didn't worked, blame someone else
    Signed-off-by: realfantacoke <CrazyMemeCoke@gmail.com>

--- minecraft/de/crazymemecoke/features/modules/movement/NoSlow.java
+++ minecraft/de/crazymemecoke/features/modules/movement/NoSlow.java
@@ -1,7 +1,9 @@
 package de.crazymemecoke.features.modules.movement;
 
 import de.crazymemecoke.manager.eventmanager.Event;
+import de.crazymemecoke.manager.eventmanager.impl.EventMotion;
 import de.crazymemecoke.manager.eventmanager.impl.EventUpdate;
+import net.minecraft.network.play.client.C08PacketPlayerBlockPlacement;
 import org.lwjgl.input.Keyboard;
 
 import de.crazymemecoke.manager.modulemanager.Category;
@@ -18,9 +20,16 @@ public class NoSlow extends Module {
 
     @Override
     public void onEvent(Event event) {
-        if (event instanceof EventUpdate) {
-            if (mc.thePlayer.isBlocking()) {
-                mc.thePlayer.sendQueue.addToSendQueue(new C07PacketPlayerDigging(C07PacketPlayerDigging.Action.RELEASE_USE_ITEM, new BlockPos(0, 0, 0), EnumFacing.DOWN));
+        if (event instanceof EventMotion) {
+            if (((EventMotion) event).getType() == EventMotion.Type.PRE) {
+                if (mc.thePlayer.isBlocking()) {
+                    mc.getNetHandler().addToSendQueue(new C07PacketPlayerDigging(C07PacketPlayerDigging.Action.RELEASE_USE_ITEM, new BlockPos(0, 0, 0), EnumFacing.UP));
+                }
+            }
+            if (((EventMotion) event).getType() == EventMotion.Type.POST) {
+                if (mc.thePlayer.isBlocking()) {
+                    mc.getNetHandler().addToSendQueue(new C08PacketPlayerBlockPlacement(mc.thePlayer.inventory.getCurrentItem()));
+                }
             }
         }
     }
