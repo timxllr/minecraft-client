d93a0dac577c70e784da44dcbd1b519a70964a5f
realfantacoke <CrazyMemeCoke@gmail.com>
2020-11-02 23:27:09 +0100

    Added "notification"-method Args: title, message, type, seconds
    Signed-off-by: realfantacoke <CrazyMemeCoke@gmail.com>

--- minecraft/de/crazymemecoke/utils/NotifyUtil.java
+++ minecraft/de/crazymemecoke/utils/NotifyUtil.java
@@ -1,6 +1,9 @@
 package de.crazymemecoke.utils;
 
 import de.crazymemecoke.Client;
+import de.crazymemecoke.manager.notificationmanager.Notification;
+import de.crazymemecoke.manager.notificationmanager.NotificationManager;
+import de.crazymemecoke.manager.notificationmanager.NotificationType;
 import net.minecraft.util.ChatComponentText;
 
 public class NotifyUtil {
@@ -13,6 +16,10 @@ public class NotifyUtil {
         if (Wrapper.mc.thePlayer != null) {
             Wrapper.mc.ingameGUI.getChatGUI().printChatMessage(new ChatComponentText("§c" + Client.main().getClientName() + " §7>> §a" + msg));
         }
+    }
+
+    public static void notification(String title, String message, NotificationType type, int seconds){
+        NotificationManager.show(new Notification(type, title, message, seconds));
     }
 
 }
