37a64d8af38bdec0b2ecc4dbf4b38f009e8e51b3
realfantacoke <CrazyMemeCoke@gmail.com>
2020-11-07 21:14:21 +0100

    Fixed NoRotation - Added if-Else to S08PacketPlayerPosLook - Removed NoFall code from NoRotation*
    * I don't even know, why that was there.
    
    Signed-off-by: realfantacoke <CrazyMemeCoke@gmail.com>

--- minecraft/de/crazymemecoke/features/modules/player/NoRotation.java
+++ minecraft/de/crazymemecoke/features/modules/player/NoRotation.java
@@ -10,31 +10,14 @@ import org.lwjgl.input.Keyboard;
 import de.crazymemecoke.manager.modulemanager.Category;
 import de.crazymemecoke.manager.modulemanager.Module;
 
-public class NoRotation extends Module{
+public class NoRotation extends Module {
 
-	public NoRotation(){
-		super("NoRotation", Keyboard.KEY_NONE, Category.PLAYER, -1);
-	}
+    public NoRotation() {
+        super("NoRotation", Keyboard.KEY_NONE, Category.PLAYER, -1);
+    }
 
 
-	@Override
-	public void onEvent(Event event) {
-		if(event instanceof EventPacket) {
-			if(((EventPacket) event).getType() == EventPacket.Type.RECEIVE) {
-				if (Client.main().setMgr().settingByName("Mode", this).getMode()
-						.equalsIgnoreCase("AAC 1.9.10")) {
-					if (!this.mc.thePlayer.onGround) {
-						Minecraft.mc().thePlayer.sendQueue.addToSendQueue(new C03PacketPlayer(true));
-						this.mc.thePlayer.onGround = true;
-						this.mc.thePlayer.fallDistance = 0.0F;
-					} else {
-						this.mc.thePlayer.onGround = false;
-						this.mc.thePlayer.onGround = true;
-					}
-				} else if (Client.main().setMgr().settingByName("Mode", this).getMode()
-						.equalsIgnoreCase("NCP")) {
-				}
-			}
-		}
-	}
+    @Override
+    public void onEvent(Event event) {
+    }
 }
--- minecraft/net/minecraft/network/play/server/S08PacketPlayerPosLook.java
+++ minecraft/net/minecraft/network/play/server/S08PacketPlayerPosLook.java
@@ -3,12 +3,14 @@ package net.minecraft.network.play.server;
 import java.io.IOException;
 import java.util.EnumSet;
 import java.util.Set;
+
+import de.crazymemecoke.Client;
+import de.crazymemecoke.features.modules.player.NoRotation;
 import net.minecraft.network.Packet;
 import net.minecraft.network.PacketBuffer;
 import net.minecraft.network.play.INetHandlerPlayClient;
 
-public class S08PacketPlayerPosLook implements Packet<INetHandlerPlayClient>
-{
+public class S08PacketPlayerPosLook implements Packet<INetHandlerPlayClient> {
     private double x;
     private double y;
     private double z;
@@ -16,12 +18,10 @@ public class S08PacketPlayerPosLook implements Packet<INetHandlerPlayClient>
     private float pitch;
     private Set<S08PacketPlayerPosLook.EnumFlags> field_179835_f;
 
-    public S08PacketPlayerPosLook()
-    {
+    public S08PacketPlayerPosLook() {
     }
 
-    public S08PacketPlayerPosLook(double xIn, double yIn, double zIn, float yawIn, float pitchIn, Set<S08PacketPlayerPosLook.EnumFlags> p_i45993_9_)
-    {
+    public S08PacketPlayerPosLook(double xIn, double yIn, double zIn, float yawIn, float pitchIn, Set<S08PacketPlayerPosLook.EnumFlags> p_i45993_9_) {
         this.x = xIn;
         this.y = yIn;
         this.z = zIn;
@@ -33,8 +33,7 @@ public class S08PacketPlayerPosLook implements Packet<INetHandlerPlayClient>
     /**
      * Reads the raw packet data from the data stream.
      */
-    public void readPacketData(PacketBuffer buf) throws IOException
-    {
+    public void readPacketData(PacketBuffer buf) throws IOException {
         this.x = buf.readDouble();
         this.y = buf.readDouble();
         this.z = buf.readDouble();
@@ -46,8 +45,7 @@ public class S08PacketPlayerPosLook implements Packet<INetHandlerPlayClient>
     /**
      * Writes the raw packet data to the data stream.
      */
-    public void writePacketData(PacketBuffer buf) throws IOException
-    {
+    public void writePacketData(PacketBuffer buf) throws IOException {
         buf.writeDouble(this.x);
         buf.writeDouble(this.y);
         buf.writeDouble(this.z);
@@ -59,43 +57,35 @@ public class S08PacketPlayerPosLook implements Packet<INetHandlerPlayClient>
     /**
      * Passes this Packet on to the NetHandler for processing.
      */
-    public void processPacket(INetHandlerPlayClient handler)
-    {
+    public void processPacket(INetHandlerPlayClient handler) {
         handler.handlePlayerPosLook(this);
     }
 
-    public double getX()
-    {
+    public double getX() {
         return this.x;
     }
 
-    public double getY()
-    {
+    public double getY() {
         return this.y;
     }
 
-    public double getZ()
-    {
+    public double getZ() {
         return this.z;
     }
 
-    public float getYaw()
-    {
+    public float getYaw() {
         return this.yaw;
     }
 
-    public float getPitch()
-    {
+    public float getPitch() {
         return this.pitch;
     }
 
-    public Set<S08PacketPlayerPosLook.EnumFlags> func_179834_f()
-    {
+    public Set<S08PacketPlayerPosLook.EnumFlags> func_179834_f() {
         return this.field_179835_f;
     }
 
-    public static enum EnumFlags
-    {
+    public static enum EnumFlags {
         X(0),
         Y(1),
         Z(2),
@@ -104,29 +94,23 @@ public class S08PacketPlayerPosLook implements Packet<INetHandlerPlayClient>
 
         private int field_180058_f;
 
-        private EnumFlags(int p_i45992_3_)
-        {
+        private EnumFlags(int p_i45992_3_) {
             this.field_180058_f = p_i45992_3_;
         }
 
-        private int func_180055_a()
-        {
+        private int func_180055_a() {
             return 1 << this.field_180058_f;
         }
 
-        private boolean func_180054_b(int p_180054_1_)
-        {
+        private boolean func_180054_b(int p_180054_1_) {
             return (p_180054_1_ & this.func_180055_a()) == this.func_180055_a();
         }
 
-        public static Set<S08PacketPlayerPosLook.EnumFlags> func_180053_a(int p_180053_0_)
-        {
+        public static Set<S08PacketPlayerPosLook.EnumFlags> func_180053_a(int p_180053_0_) {
             Set<S08PacketPlayerPosLook.EnumFlags> set = EnumSet.<S08PacketPlayerPosLook.EnumFlags>noneOf(S08PacketPlayerPosLook.EnumFlags.class);
 
-            for (S08PacketPlayerPosLook.EnumFlags s08packetplayerposlook$enumflags : values())
-            {
-                if (s08packetplayerposlook$enumflags.func_180054_b(p_180053_0_))
-                {
+            for (S08PacketPlayerPosLook.EnumFlags s08packetplayerposlook$enumflags : values()) {
+                if (s08packetplayerposlook$enumflags.func_180054_b(p_180053_0_)) {
                     set.add(s08packetplayerposlook$enumflags);
                 }
             }
@@ -134,12 +118,10 @@ public class S08PacketPlayerPosLook implements Packet<INetHandlerPlayClient>
             return set;
         }
 
-        public static int func_180056_a(Set<S08PacketPlayerPosLook.EnumFlags> p_180056_0_)
-        {
+        public static int func_180056_a(Set<S08PacketPlayerPosLook.EnumFlags> p_180056_0_) {
             int i = 0;
 
-            for (S08PacketPlayerPosLook.EnumFlags s08packetplayerposlook$enumflags : p_180056_0_)
-            {
+            for (S08PacketPlayerPosLook.EnumFlags s08packetplayerposlook$enumflags : p_180056_0_) {
                 i |= s08packetplayerposlook$enumflags.func_180055_a();
             }
 
@@ -148,10 +130,14 @@ public class S08PacketPlayerPosLook implements Packet<INetHandlerPlayClient>
     }
 
     public void setPitch(float pitch) {
-        this.pitch = pitch;
+        if (!(Client.main().modMgr().getModule(NoRotation.class)).state()) {
+            this.pitch = pitch;
+        }
     }
 
     public void setYaw(float yaw) {
-        this.yaw = yaw;
+        if (!(Client.main().modMgr().getModule(NoRotation.class)).state()) {
+            this.yaw = yaw;
+        }
     }
 }
