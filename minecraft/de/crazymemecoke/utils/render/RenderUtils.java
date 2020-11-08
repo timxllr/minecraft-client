276357924e7152811d1f23a722d66144f5fa6882
realfantacoke <CrazyMemeCoke@gmail.com>
2020-11-08 01:33:01 +0100

    Added removeColorNode method to RenderUtils
    Signed-off-by: realfantacoke <CrazyMemeCoke@gmail.com>

--- minecraft/de/crazymemecoke/utils/render/RenderUtils.java
+++ minecraft/de/crazymemecoke/utils/render/RenderUtils.java
@@ -22,6 +22,17 @@ import static org.lwjgl.opengl.GL11.*;
 
 public class RenderUtils {
 
+    private static final Vec3 field_82884_b = new Vec3(0.20000000298023224D, 1.0D, -0.699999988079071D).normalize();
+    private static final Vec3 field_82885_c = new Vec3(-0.20000000298023224D, 1.0D, 0.699999988079071D).normalize();
+    private static final String __OBFID = "CL_00000629";
+    private static final ScaledResolution sr = new ScaledResolution(Minecraft.mc());
+    public static int enemy = 0;
+    public static int friend = 1;
+    public static int other = 2;
+    public static int target = 3;
+    public static int team = 4;
+    private static FloatBuffer colorBuffer = GLAllocation.createDirectFloatBuffer(16);
+
     /**
      * Defines a rectangle (scissorBox) in window coordinates not GL's: from
      * https://vinii.de/github/LWJGLUtil/scissorBoxGL.png to
@@ -135,7 +146,6 @@ public class RenderUtils {
     }
 
     /**
-     *
      * Draws a horizontal gradient rect
      */
     public static void drawHorizontalGradient(final float x, final float y, final float width, final float height,
@@ -201,12 +211,6 @@ public class RenderUtils {
         GL11.glShadeModel(7424);
     }
 
-    public static int enemy = 0;
-    public static int friend = 1;
-    public static int other = 2;
-    public static int target = 3;
-    public static int team = 4;
-
     public static void color(final int color) {
         final float f = (color >> 24 & 0xFF) / 255.0f;
         final float f2 = (color >> 16 & 0xFF) / 255.0f;
@@ -223,21 +227,12 @@ public class RenderUtils {
         return (new Color(r, g, b, alpha)).getRGB();
     }
 
-    private static FloatBuffer colorBuffer = GLAllocation.createDirectFloatBuffer(16);
-    private static final Vec3 field_82884_b = new Vec3(0.20000000298023224D, 1.0D, -0.699999988079071D).normalize();
-    private static final Vec3 field_82885_c = new Vec3(-0.20000000298023224D, 1.0D, 0.699999988079071D).normalize();
-
-    private static final String __OBFID = "CL_00000629";
-    private static final ScaledResolution sr = new ScaledResolution(Minecraft.mc());
-
-    public static final ScaledResolution getScaledRes()
-    {
+    public static final ScaledResolution getScaledRes() {
         ScaledResolution scaledRes = new ScaledResolution(Minecraft.mc());
         return scaledRes;
     }
 
-    public static void enableStandardItemLighting()
-    {
+    public static void enableStandardItemLighting() {
         GlStateManager.enableLighting();
         GlStateManager.enableLight(0);
         GlStateManager.enableLight(1);
@@ -259,7 +254,7 @@ public class RenderUtils {
     }
 
     private static FloatBuffer setColorBuffer(double p_74517_0_, double p_74517_2_, double p_74517_4_, double p_74517_6_) {
-        return setColorBuffer((float)p_74517_0_, (float)p_74517_2_, (float)p_74517_4_, (float)p_74517_6_);
+        return setColorBuffer((float) p_74517_0_, (float) p_74517_2_, (float) p_74517_4_, (float) p_74517_6_);
     }
 
     public static void drawTexturedRect(int x, int y, int width, int height, GuiIngame screen, ResourceLocation texture) {
@@ -276,19 +271,15 @@ public class RenderUtils {
         GlStateManager.popMatrix();
     }
 
-    public static void drawHollowRect(float posX, float posY, float posX2, float posY2, float width, int color, boolean center)
-    {
+    public static void drawHollowRect(float posX, float posY, float posX2, float posY2, float width, int color, boolean center) {
         float corners = width / 2.0F;
         float side = width / 2.0F;
-        if (center)
-        {
+        if (center) {
             drawRect(posX - side, posY - corners, posX + side, posY2 + corners, color);
             drawRect(posX2 - side, posY - corners, posX2 + side, posY2 + corners, color);
             drawRect(posX - corners, posY - side, posX2 + corners, posY + side, color);
             drawRect(posX - corners, posY2 - side, posX2 + corners, posY2 + side, color);
-        }
-        else
-        {
+        } else {
             drawRect(posX - width, posY - corners, posX, posY2 + corners, color);
             drawRect(posX2, posY - corners, posX2 + width, posY2 + corners, color);
             drawRect(posX - corners, posY - width, posX2 + corners, posY, color);
@@ -296,14 +287,12 @@ public class RenderUtils {
         }
     }
 
-    public static void drawGradientBorderedRect(float posX, float posY, float posX2, float posY2, float width, int color, int startColor, int endColor, boolean center)
-    {
+    public static void drawGradientBorderedRect(float posX, float posY, float posX2, float posY2, float width, int color, int startColor, int endColor, boolean center) {
         drawGradientRect(posX, posY, posX2, posY2, startColor, endColor);
         drawHollowRect(posX, posY, posX2, posY2, width, color, center);
     }
 
-    public static void drawCoolLines(AxisAlignedBB mask)
-    {
+    public static void drawCoolLines(AxisAlignedBB mask) {
         GL11.glPushMatrix();
         GL11.glBegin(2);
         GL11.glVertex3d(mask.minX, mask.minY,
@@ -344,8 +333,7 @@ public class RenderUtils {
         GL11.glPopMatrix();
     }
 
-    public static void drawBorderedRect(float x, float y, float x2, float y2, float l1, int col1, int col2)
-    {
+    public static void drawBorderedRect(float x, float y, float x2, float y2, float l1, int col1, int col2) {
         drawRect(x, y, x2, y2, col2);
 
         float f = (col1 >> 24 & 0xFF) / 255.0F;
@@ -378,8 +366,7 @@ public class RenderUtils {
         GL11.glDisable(2848);
     }
 
-    public static void drawBorderedCorneredRect(float x, float y, float x2, float y2, float lineWidth, int lineColor, int bgColor)
-    {
+    public static void drawBorderedCorneredRect(float x, float y, float x2, float y2, float lineWidth, int lineColor, int bgColor) {
         drawRect(x, y, x2, y2, bgColor);
 
         float f = (lineColor >> 24 & 0xFF) / 255.0F;
@@ -397,8 +384,7 @@ public class RenderUtils {
         GL11.glDisable(3042);
     }
 
-    public static double interp(double from, double to, double pct)
-    {
+    public static double interp(double from, double to, double pct) {
         return from + (to - from) * pct;
     }
 
@@ -516,13 +502,11 @@ public class RenderUtils {
     tessellator.draw();
   }*/
 
-    public static void glColor(Color color)
-    {
+    public static void glColor(Color color) {
         GL11.glColor4f(color.getRed() / 255.0F, color.getGreen() / 255.0F, color.getBlue() / 255.0F, color.getAlpha() / 255.0F);
     }
 
-    public static void glColor(int hex)
-    {
+    public static void glColor(int hex) {
         float alpha = (hex >> 24 & 0xFF) / 255.0F;
         float red = (hex >> 16 & 0xFF) / 255.0F;
         float green = (hex >> 8 & 0xFF) / 255.0F;
@@ -530,8 +514,7 @@ public class RenderUtils {
         GL11.glColor4f(red, green, blue, alpha);
     }
 
-    public static void drawGradientRect(float x, float y, float x1, float y1, int topColor, int bottomColor)
-    {
+    public static void drawGradientRect(float x, float y, float x1, float y1, int topColor, int bottomColor) {
         GL11.glEnable(1536);
         GL11.glShadeModel(7425);
         GL11.glBegin(7);
@@ -546,8 +529,7 @@ public class RenderUtils {
         GL11.glDisable(1536);
     }
 
-    public static void drawLines(AxisAlignedBB mask)
-    {
+    public static void drawLines(AxisAlignedBB mask) {
         GL11.glPushMatrix();
         GL11.glBegin(2);
         GL11.glVertex3d(mask.minX, mask.minY,
@@ -672,8 +654,7 @@ public class RenderUtils {
     var1.draw();
   }*/
 
-    public static void drawRect(float g, float h, float i, float j, int col1)
-    {
+    public static void drawRect(float g, float h, float i, float j, int col1) {
         float f = (col1 >> 24 & 0xFF) / 255.0F;
         float f1 = (col1 >> 16 & 0xFF) / 255.0F;
         float f2 = (col1 >> 8 & 0xFF) / 255.0F;
@@ -699,8 +680,7 @@ public class RenderUtils {
         GL11.glDisable(2848);
     }
 
-    public static void drawRect(float g, float h, float i, float j, Color col2)
-    {
+    public static void drawRect(float g, float h, float i, float j, Color col2) {
         ColorUtil.setColor(col2);
 
         GL11.glEnable(3042);
@@ -722,8 +702,7 @@ public class RenderUtils {
         GL11.glDisable(2848);
     }
 
-    public static void drawButtonBorderedRect(float x1, float y1, float x2, float y2, float x3, float y3, float x4, float y4, Color innerColor, Color outerColor, float lineWidhth)
-    {
+    public static void drawButtonBorderedRect(float x1, float y1, float x2, float y2, float x3, float y3, float x4, float y4, Color innerColor, Color outerColor, float lineWidhth) {
         drawButtonRect(x1, y1, x2, y2, x3, y3, x4, y4, innerColor);
 
         GL11.glEnable(3042);
@@ -748,8 +727,7 @@ public class RenderUtils {
         GL11.glColor4f(0.0F, 0.0F, 0.0F, 0.0F);
     }
 
-    public static void drawButtonRect(float x1, float y1, float x2, float y2, float x3, float y3, float x4, float y4, Color col)
-    {
+    public static void drawButtonRect(float x1, float y1, float x2, float y2, float x3, float y3, float x4, float y4, Color col) {
         GL11.glEnable(3042);
         GL11.glDisable(3553);
         GL11.glBlendFunc(770, 771);
@@ -1217,6 +1195,23 @@ public class RenderUtils {
         worldRenderer.pos(aa.minX, aa.minY, aa.maxZ);
         worldRenderer.pos(aa.minX, aa.maxY, aa.maxZ);
         tessellator.draw();
+    }
+
+    public static String removeColorCode(String text) {
+        String finalText = text;
+        if (finalText.contains("§")) {
+            for (int i = 0; i < finalText.length(); i++) {
+                if (Character.toString(finalText.charAt(i)).equals("�")) {
+                    try {
+                        String part1 = finalText.substring(0, i);
+                        String part2 = finalText.substring(Math.min(i + 2, finalText.length()));
+                        finalText = part1 + part2;
+                    } catch (Exception ignored) {
+                    }
+                }
+            }
+        }
+        return finalText;
     }
 
     public static void drawBoundingBox(AxisAlignedBB aa) {
