64716210c87fe78df5cefd84bd89d66b33dbf5d5
realfantacoke <CrazyMemeCoke@gmail.com>
2020-11-08 22:56:45 +0100

    Added Delay option to AutoRespawn
    Signed-off-by: realfantacoke <CrazyMemeCoke@gmail.com>

--- minecraft/de/crazymemecoke/features/modules/player/AutoRespawn.java
+++ minecraft/de/crazymemecoke/features/modules/player/AutoRespawn.java
@@ -1,21 +1,37 @@
 package de.crazymemecoke.features.modules.player;
 
+import de.crazymemecoke.Client;
+import de.crazymemecoke.manager.clickguimanager.settings.Setting;
 import de.crazymemecoke.manager.eventmanager.Event;
 import de.crazymemecoke.manager.eventmanager.impl.EventUpdate;
 import de.crazymemecoke.manager.modulemanager.Category;
 import de.crazymemecoke.manager.modulemanager.Module;
+import de.crazymemecoke.utils.time.TimeHelper;
 import org.lwjgl.input.Keyboard;
 
 public class AutoRespawn extends Module {
+    TimeHelper timeHelper = new TimeHelper();
+
     public AutoRespawn() {
-        super("AutoRespawn", Keyboard.KEY_NONE, Category.PLAYER, -1);
+        super("AutoRespawn", Keyboard.KEY_NONE, Category.PLAYER);
+
+        Client.main().setMgr().newSetting(new Setting("Delay", this, 0, 0, 1000, false));
+    }
+
+    @Override
+    public void onDisable() {
+        timeHelper.reset();
     }
 
     @Override
     public void onEvent(Event event) {
-        if(event instanceof EventUpdate) {
+        if (event instanceof EventUpdate) {
             if (!(mc.thePlayer.isEntityAlive())) {
-                mc.thePlayer.respawnPlayer();
+                double delay = Client.main().setMgr().settingByName("Delay", this).getNum();
+                if (timeHelper.isDelayComplete((long) delay)) {
+                    mc.thePlayer.respawnPlayer();
+                    timeHelper.reset();
+                }
             }
         }
     }
