74647b416ae0bd102efa8e1c43c7b5923b7949d9
realfantacoke <CrazyMemeCoke@gmail.com>
2020-11-08 01:31:01 +0100

    Recoded NoFall - Removed NCP mode - Added AAC 3.3.8 mode - Added AAC 3.3.11 mode - Added Vanilla mode
    Signed-off-by: realfantacoke <CrazyMemeCoke@gmail.com>

--- minecraft/de/crazymemecoke/features/modules/player/NoFall.java
+++ minecraft/de/crazymemecoke/features/modules/player/NoFall.java
@@ -1,17 +1,16 @@
 package de.crazymemecoke.features.modules.player;
 
-import java.util.ArrayList;
-
+import de.crazymemecoke.Client;
+import de.crazymemecoke.manager.clickguimanager.settings.Setting;
 import de.crazymemecoke.manager.eventmanager.Event;
 import de.crazymemecoke.manager.eventmanager.impl.EventUpdate;
-import org.lwjgl.input.Keyboard;
-
-import de.crazymemecoke.manager.clickguimanager.settings.Setting;
-import de.crazymemecoke.Client;
 import de.crazymemecoke.manager.modulemanager.Category;
 import de.crazymemecoke.manager.modulemanager.Module;
 import net.minecraft.client.Minecraft;
 import net.minecraft.network.play.client.C03PacketPlayer;
+import org.lwjgl.input.Keyboard;
+
+import java.util.ArrayList;
 
 public class NoFall extends Module {
 
@@ -20,28 +19,69 @@ public class NoFall extends Module {
     public NoFall() {
         super("NoFall", Keyboard.KEY_NONE, Category.PLAYER, -1);
 
+        mode.add("AAC 3.3.11");
+        mode.add("AAC 3.3.8");
         mode.add("AAC 1.9.10");
-        mode.add("NCP");
+        mode.add("Vanilla");
 
         Client.main().setMgr().newSetting(new Setting("Mode", this, "AAC 1.9.10", mode));
     }
 
     @Override
     public void onEvent(Event event) {
-        if(event instanceof EventUpdate) {
-            if (Client.main().setMgr().settingByName("Mode", this).getMode()
-                    .equalsIgnoreCase("AAC 1.9.10")) {
-                if (!this.mc.thePlayer.onGround) {
-                    Minecraft.mc().thePlayer.sendQueue.addToSendQueue(new C03PacketPlayer(true));
-                    this.mc.thePlayer.onGround = true;
-                    this.mc.thePlayer.fallDistance = 0.0F;
-                } else {
-                    this.mc.thePlayer.onGround = false;
-                    this.mc.thePlayer.onGround = true;
+        if (event instanceof EventUpdate) {
+            String mode = Client.main().setMgr().settingByName("Mode", this).getMode();
+
+            switch (mode) {
+                case "vanilla": {
+                    doVanilla();
+                    break;
                 }
-            } else if (Client.main().setMgr().settingByName("Mode", this).getMode()
-                    .equalsIgnoreCase("NCP")) {
+                case "aac 1.9.10": {
+                    doAAC1910();
+                    break;
+                }
+                case "aac 3.3.8": {
+                    doAAC338();
+                    break;
+                }
+                case "aac 3.3.11": {
+                    doAAC3311();
+                    break;
+                }
             }
+        }
+    }
+
+    private void doAAC3311() {
+        if (mc.thePlayer.fallDistance > 2.0F) {
+            mc.thePlayer.motionX = (mc.thePlayer.motionZ = 0.0D);
+            mc.thePlayer.sendQueue.addToSendQueue(new C03PacketPlayer.C04PacketPlayerPosition(mc.thePlayer.posX, mc.thePlayer.posY - 0.001D, mc.thePlayer.posZ, mc.thePlayer.onGround));
+            mc.thePlayer.sendQueue.addToSendQueue(new C03PacketPlayer(true));
+        }
+    }
+
+    private void doAAC338() {
+        if (mc.thePlayer.onGround) {
+            mc.thePlayer.motionY = -9.0D;
+        }
+    }
+
+    private void doAAC1910() {
+        if (!mc.thePlayer.onGround) {
+            Minecraft.mc().thePlayer.sendQueue.addToSendQueue(new C03PacketPlayer(true));
+            mc.thePlayer.onGround = true;
+            mc.thePlayer.fallDistance = 0.0F;
+        } else {
+            mc.thePlayer.onGround = false;
+            mc.thePlayer.onGround = true;
+        }
+    }
+
+    private void doVanilla() {
+        if (mc.thePlayer.fallDistance > 2.0F) {
+            mc.thePlayer.sendQueue.addToSendQueue(new C03PacketPlayer(true));
+            mc.thePlayer.fallDistance = 0.0F;
         }
     }
 }
