6138ea35566411f5783defd0f9ae71d498927e59
realfantacoke <CrazyMemeCoke@gmail.com>
2020-11-29 15:20:02 +0100

    Added TimeHelper and onEvent to Command
    Signed-off-by: realfantacoke <CrazyMemeCoke@gmail.com>

--- minecraft/de/crazymemecoke/features/commands/Command.java
+++ minecraft/de/crazymemecoke/features/commands/Command.java
@@ -1,15 +1,24 @@
 package de.crazymemecoke.features.commands;
 
+import de.crazymemecoke.Methods;
+import de.crazymemecoke.manager.eventmanager.Event;
+import de.crazymemecoke.utils.time.TimeHelper;
 import net.minecraft.client.Minecraft;
 
-public abstract class Command {
+public abstract class Command extends Methods {
+
+	public TimeHelper timeHelper = new TimeHelper();
 
 	public static Minecraft mc = Minecraft.mc();
 	
 	public abstract void execute(String[] args);
 	
 	public abstract String getName();
-	
+
+	public void onEvent(Event event) {
+
+	}
+
 	private String label;
 	
 	  public String[] getArguments()
