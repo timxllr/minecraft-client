cac6b7edaaf12362068665f25a023ee0d72e7c14
realfantacoke <CrazyMemeCoke@gmail.com>
2020-11-29 16:07:48 +0100

    Added NCP FastBow & Mode selector
    Signed-off-by: realfantacoke <CrazyMemeCoke@gmail.com>

--- minecraft/de/crazymemecoke/features/modules/impl/combat/FastBow.java
+++ minecraft/de/crazymemecoke/features/modules/impl/combat/FastBow.java
@@ -1,10 +1,11 @@
 package de.crazymemecoke.features.modules.impl.combat;
 
-import de.crazymemecoke.manager.eventmanager.Event;
-import de.crazymemecoke.manager.eventmanager.impl.EventUpdate;
 import de.crazymemecoke.features.modules.Category;
 import de.crazymemecoke.features.modules.Module;
 import de.crazymemecoke.features.modules.ModuleInfo;
+import de.crazymemecoke.manager.eventmanager.Event;
+import de.crazymemecoke.manager.eventmanager.impl.EventUpdate;
+import de.crazymemecoke.manager.settingsmanager.Setting;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemBow;
 import net.minecraft.item.ItemStack;
@@ -15,6 +16,8 @@ import net.minecraft.util.EnumFacing;
 
 @ModuleInfo(name = "FastBow", category = Category.COMBAT, description = "You shoot your arrows like ratatatatam")
 public class FastBow extends Module {
+
+    public Setting mode = new Setting("Mode", this, "Vanilla", new String[]{"Vanilla", "NCP"});
 
     @Override
     public void onToggle() {
@@ -36,19 +39,41 @@ public class FastBow extends Module {
         if (event instanceof EventUpdate) {
             if (mc.thePlayer.inventory.getCurrentItem() != null) {
                 if (mc.thePlayer.inventory.getCurrentItem().getItem() instanceof ItemBow && mc.gameSettings.keyBindUseItem.pressed) {
-                    mc.playerController.sendUseItem(mc.thePlayer, mc.theWorld, mc.thePlayer.inventory.getCurrentItem());
-                    Item item = mc.thePlayer.inventory.getCurrentItem().getItem();
-                    ItemStack itemStack = mc.thePlayer.inventory.getCurrentItem();
-                    item.onItemRightClick(itemStack, mc.theWorld, mc.thePlayer);
-                    for (int i = 0; i < 20; ++i) {
-                        mc.thePlayer.sendQueue.addToSendQueue(new C03PacketPlayer(false));
+                    switch (mode.getCurrentMode()) {
+                        case "Vanilla": {
+                            doVanilla();
+                            break;
+                        }
+                        case "NCP": {
+                            doNCP();
+                            break;
+                        }
                     }
-                    mc.getNetHandler().addToSendQueue(new C07PacketPlayerDigging(C07PacketPlayerDigging.Action.RELEASE_USE_ITEM, new BlockPos(0, 0, 0), EnumFacing.DOWN));
-                    item = mc.thePlayer.inventory.getCurrentItem().getItem();
-                    itemStack = mc.thePlayer.inventory.getCurrentItem();
-                    item.onPlayerStoppedUsing(itemStack, mc.theWorld, mc.thePlayer, 10);
                 }
             }
         }
+    }
+
+    private void doNCP() {
+        if (getPlayer().getItemInUseDuration() >= 15) {
+            for (int loop = 0; loop < 25; loop++) {
+                mc.thePlayer.sendQueue.addToSendQueue(new C03PacketPlayer(mc.thePlayer.onGround));
+            }
+            mc.playerController.onStoppedUsingItem(mc.thePlayer);
+        }
+    }
+
+    private void doVanilla() {
+        getPlayerController().sendUseItem(mc.thePlayer, mc.theWorld, mc.thePlayer.inventory.getCurrentItem());
+        Item item = getPlayer().inventory.getCurrentItem().getItem();
+        ItemStack itemStack = getPlayer().inventory.getCurrentItem();
+        item.onItemRightClick(itemStack, mc.theWorld, mc.thePlayer);
+        for (int i = 0; i < 20; ++i) {
+            mc.thePlayer.sendQueue.addToSendQueue(new C03PacketPlayer(false));
+        }
+        mc.getNetHandler().addToSendQueue(new C07PacketPlayerDigging(C07PacketPlayerDigging.Action.RELEASE_USE_ITEM, new BlockPos(0, 0, 0), EnumFacing.DOWN));
+        item = mc.thePlayer.inventory.getCurrentItem().getItem();
+        itemStack = mc.thePlayer.inventory.getCurrentItem();
+        item.onPlayerStoppedUsing(itemStack, mc.theWorld, mc.thePlayer, 10);
     }
 }
