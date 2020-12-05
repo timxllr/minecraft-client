91517f4a61b4b6e104009a9d98e9c87aa9f5d3c0
realfantacoke <CrazyMemeCoke@gmail.com>
2020-12-05 20:38:29 +0100

    Added cmd aliases
    Signed-off-by: realfantacoke <CrazyMemeCoke@gmail.com>

--- minecraft/com/masterof13fps/features/commands/CommandManager.java
+++ minecraft/com/masterof13fps/features/commands/CommandManager.java
@@ -2,6 +2,7 @@ package com.masterof13fps.features.commands;
 
 import com.masterof13fps.Client;
 import com.masterof13fps.features.commands.impl.*;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -11,6 +12,7 @@ public class CommandManager {
     public List<Command> commands = new ArrayList<Command>();
 
     public CommandManager() {
+        commands.add(new Register());
         commands.add(new Enchant());
         commands.add(new Toggle());
         commands.add(new Rename());
@@ -18,6 +20,7 @@ public class CommandManager {
         commands.add(new Vanish());
         commands.add(new Reload());
         commands.add(new Panic());
+        commands.add(new Login());
         commands.add(new Bind());
         commands.add(new Help());
         commands.add(new Info());
@@ -25,6 +28,7 @@ public class CommandManager {
         commands.add(new Fix());
         commands.add(new Say());
         commands.add(new NCP());
+        commands.add(new IGN());
     }
 
     public boolean execute(String text) {
@@ -35,7 +39,7 @@ public class CommandManager {
         text = text.substring(1);
         String[] arguments = text.split(" ");
         for (Command cmd : commands) {
-            if (cmd.getName().equalsIgnoreCase(arguments[0])) {
+            if (cmd.getName().equalsIgnoreCase(arguments[0]) || cmd.getAlias().equalsIgnoreCase(arguments[0])) {
                 String[] args = Arrays.copyOfRange(arguments, 1, arguments.length);
                 cmd.execute(args);
                 return true;
