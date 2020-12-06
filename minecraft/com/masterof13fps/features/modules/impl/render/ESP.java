17dbf8c7066fa89589072636ec49802cc56aebc4
realfantacoke <CrazyMemeCoke@gmail.com>
2020-12-06 03:26:20 +0100

    Reworked ESP - Added Tornado mode - Removed not-working modes from selection (code still available inside the class file) - Removed Murder mode (made an extra module for that w/o ESP)
    Signed-off-by: realfantacoke <CrazyMemeCoke@gmail.com>

--- minecraft/com/masterof13fps/features/modules/impl/render/ESP.java
+++ minecraft/com/masterof13fps/features/modules/impl/render/ESP.java
@@ -1,24 +1,20 @@
 package com.masterof13fps.features.modules.impl.render;
 
+import com.masterof13fps.features.modules.Category;
 import com.masterof13fps.features.modules.Module;
 import com.masterof13fps.features.modules.ModuleInfo;
-import com.masterof13fps.utils.NotifyUtil;
-import com.masterof13fps.features.modules.Category;
 import com.masterof13fps.manager.eventmanager.Event;
 import com.masterof13fps.manager.eventmanager.impl.EventOutline;
 import com.masterof13fps.manager.eventmanager.impl.EventRender;
-import com.masterof13fps.manager.eventmanager.impl.EventUpdate;
-import com.masterof13fps.manager.notificationmanager.NotificationType;
 import com.masterof13fps.manager.settingsmanager.Setting;
+import com.masterof13fps.utils.render.Rainbow;
 import com.masterof13fps.utils.render.RenderUtils;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.entity.RenderManager;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityLiving;
 import net.minecraft.entity.EntityLivingBase;
-import net.minecraft.entity.item.EntityItem;
 import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.item.ItemSword;
 import net.minecraft.util.MathHelper;
 import org.lwjgl.opengl.GL11;
 
@@ -27,14 +23,16 @@ import java.awt.*;
 @ModuleInfo(name = "ESP", category = Category.RENDER, description = "Shows selected targets")
 public class ESP extends Module {
 
-    public Setting mode = new Setting("Mode", this, "Shader", new String[]{"Shader", "Outline", "Murder", "Prophunt", "Box"});
-    public Setting players = new Setting("Players", this, true);
-    public Setting mobs = new Setting("Mobs", this, false);
-    public Setting animals = new Setting("Animals", this, false);
-    public Setting villager = new Setting("Villager", this, false);
-    public Setting items = new Setting("Items", this, false);
+    Setting mode = new Setting("Mode", this, "Shader", new String[]{"Shader", "Tornado"});
+    Setting tornadoWidth = new Setting("Tornado Width", this, 0.7, 0.5, 1, false);
+    Setting tornadoAnimationSpeed = new Setting("Tornado Animation Speed", this, 20, 0, 60, true);
+    Setting players = new Setting("Players", this, true);
+    Setting mobs = new Setting("Mobs", this, false);
+    Setting animals = new Setting("Animals", this, false);
+    Setting villager = new Setting("Villager", this, false);
+    Setting items = new Setting("Items", this, false);
 
-    public EntityPlayer murderer;
+    private int animationX;
 
     @Override
     public void onToggle() {
@@ -48,7 +46,6 @@ public class ESP extends Module {
 
     @Override
     public void onDisable() {
-        murderer = null;
     }
 
     public void player(EntityLivingBase entity) {
@@ -75,12 +72,12 @@ public class ESP extends Module {
         if (event instanceof EventRender) {
             if (((EventRender) event).getType() == EventRender.Type.threeD) {
                 switch (mode.getCurrentMode()) {
-                    case "Murder": {
-                        doMurder();
-                        break;
-                    }
-                    case "Prophunt": {
+                    /*case "Prophunt": {
                         doProphunt();
+                        break;
+                    }*/
+                    case "Tornado": {
+                        doTornado();
                         break;
                     }
                 }
@@ -95,52 +92,60 @@ public class ESP extends Module {
                 }
             }
         }
-
-        if (event instanceof EventUpdate) {
-            if (murderer == null) {
-                for (Entity e : mc.theWorld.loadedEntityList) {
-                    if (((e instanceof EntityPlayer)) && (((EntityPlayer) e).getCurrentEquippedItem() != null) && ((((EntityPlayer) e).getCurrentEquippedItem().getItem() instanceof ItemSword))) {
-                        NotifyUtil.notification("Mörder erkannt!", "Achtung! " + e.getName() + " ist der Mörder!", NotificationType.INFO, 5);
-                        murderer = (EntityPlayer) e;
-                    }
-                }
-            }
-        }
     }
 
-    private void doMurder() {
-        if (murderer != null) {
-            float x = (float) (murderer.lastTickPosX + (murderer.posX - murderer.lastTickPosX) * mc.timer.renderPartialTicks - RenderManager.renderPosX);
-            float y = (float) (murderer.lastTickPosY + (murderer.posY - murderer.lastTickPosY) * mc.timer.renderPartialTicks - RenderManager.renderPosY);
-            float z = (float) (murderer.lastTickPosZ + (murderer.posZ - murderer.lastTickPosZ) * mc.timer.renderPartialTicks - RenderManager.renderPosZ);
-            GL11.glColor3f(0.5F, 0.0F, 0.0F);
-            GL11.glColor3f(1.0F, 1.0F, 1.0F);
+    private void doTornado() {
+
+        GL11.glPushMatrix();
+        GL11.glDisable(GL11.GL_TEXTURE_2D);
+        //GL11.glDisable(GL11.GL_DEPTH_TEST);
+        GL11.glEnable(GL11.GL_LINE_SMOOTH);
+
+        if (timeHelper.hasReached((long) tornadoAnimationSpeed.getCurrentValue())) {
+            animationX++;
+            timeHelper.reset();
         }
-        for (Entity e : mc.theWorld.loadedEntityList) {
-            if ((e instanceof EntityItem)) {
-                EntityItem item = (EntityItem) e;
-                float x = (float) (item.lastTickPosX + (item.posX - item.lastTickPosX) * mc.timer.renderPartialTicks - RenderManager.renderPosX);
-                float y = (float) (item.lastTickPosY + (item.posY - item.lastTickPosY) * mc.timer.renderPartialTicks - RenderManager.renderPosY);
-                float z = (float) (item.lastTickPosZ + (item.posZ - item.lastTickPosZ) * mc.timer.renderPartialTicks - RenderManager.renderPosZ);
-                GL11.glPushMatrix();
-                GL11.glEnable(3042);
-                GL11.glBlendFunc(770, 771);
-                GL11.glDisable(3553);
-                GL11.glEnable(2848);
-                GL11.glDisable(2929);
-                GL11.glDepthMask(false);
-                GL11.glTranslated(x, y, z);
-                GL11.glTranslated(-x, -y, -z);
-                GL11.glColor4f(0.9F, 0.76F, 0.0F, 0.5F);
-                RenderUtils.box(x - 0.2D, y + 0.1D, z - 0.2D, x + 0.2D, y + 0.5D, z + 0.2D, new Color(0,0,0));
-                GL11.glDisable(2848);
-                GL11.glEnable(3553);
-                GL11.glEnable(2929);
-                GL11.glDepthMask(true);
-                GL11.glDisable(3042);
-                GL11.glPopMatrix();
+
+        for (Entity entity : getWorld().loadedEntityList) {
+            int currentOffset = 0;
+            float animationY = 0.0f;
+            float animationY2 = 0.0f;
+            if (entity != getPlayer() && entity instanceof EntityPlayer) {
+                translateRotate(entity);
+                GL11.glLineWidth(1);
+                GL11.glBegin(GL11.GL_LINE_STRIP);
+
+                for (int i = animationX; i < 100 + animationX; i++) {
+                    double c = (2 * i * Math.PI / 100);
+                    Color color = Rainbow.getRainbow(currentOffset, 1000, 1f, 1f);
+                    GL11.glColor3d(color.getRed() / 255f, color.getGreen() / 255f, color.getBlue() / 255f);
+                    GL11.glVertex3d((Math.cos(c) * tornadoWidth.getCurrentValue()), animationY,
+                            (Math.sin(c) * tornadoWidth.getCurrentValue()));
+                    animationY += 0.02f;
+                    currentOffset += 10;
+                }
+                GL11.glEnd();
+
+                GL11.glBegin(GL11.GL_LINE_STRIP);
+
+                for (int i = 50 + animationX; i < 150 + animationX; i++) {
+                    double c = (2 * i * Math.PI / 100);
+                    Color color = Rainbow.getRainbow(currentOffset, 1000, 1f, 1f);
+                    GL11.glColor3d(color.getRed() / 255f, color.getGreen() / 255f, color.getBlue() / 255f);
+                    GL11.glVertex3d((Math.cos(c) * tornadoWidth.getCurrentValue()), animationY2,
+                            (Math.sin(c) * tornadoWidth.getCurrentValue()));
+                    animationY2 += 0.02f;
+                    currentOffset += 10;
+                }
+                GL11.glEnd();
             }
         }
+
+        GL11.glDisable(GL11.GL_LINE_SMOOTH);
+        //GL11.glEnable(GL11.GL_DEPTH_TEST);
+        GL11.glEnable(GL11.GL_TEXTURE_2D);
+        GL11.glPopMatrix();
+
     }
 
     private void doProphunt() {
@@ -159,5 +164,16 @@ public class ESP extends Module {
                     color = new Color(0, 0, 0, 0);
                 RenderUtils.box(x - 0.5, y - 0.1, z - 0.5, x + 0.5, y + 0.9, z + 0.5, color);
             }
+    }
+
+    public void translateRotate(Entity entity) {
+        double x =
+                entity.lastTickPosX + (entity.posX - entity.lastTickPosX) * mc.timer.renderPartialTicks - mc.getRenderManager().renderPosX;
+        double y =
+                entity.lastTickPosY + (entity.posY - entity.lastTickPosY) * mc.timer.renderPartialTicks - mc.getRenderManager().renderPosY;
+        double z =
+                entity.lastTickPosZ + (entity.posZ - entity.lastTickPosZ) * mc.timer.renderPartialTicks - mc.getRenderManager().renderPosZ;
+        GL11.glTranslated(x, y, z);
+        GL11.glNormal3d(0.0, 0.0, 0.0);
     }
 }
